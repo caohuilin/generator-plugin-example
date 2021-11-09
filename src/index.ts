6fd1c2ff71884fec2f6a4c17a082723bc8f49cce
caohuilin <caohuilin@bytedance.com>
2021-11-09 16:09:23 +0800

    feat: plugin info

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export default function () {
-  return 'hello world';
+export default function (context: any) {
+  context.setSolutionInfo({
+    key: 'test',
+    name: '测试',
+    base: 'mwa',
+  });
+  context.addInput({
+    key: 'test',
+    label: 'test',
+    isObject: true,
+    items: [
+      {
+        key: 'test1',
+        label: 'test1',
+        type: ['string'],
+      },
+    ],
+  });
 }
